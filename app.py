6a76ea328d4304de9fb9a74ae91b217495c7055d
Hernan Miranda <203705774+NHFM4@users.noreply.github.com>
2025-07-08 23:56:49 -0400

    Casassacas

--- app.py
+++ app.py
@@ -10,13 +10,49 @@ diccionario_de_busquedas = {
     "fecha": connect.busqueda_por_fecha,
     "producto": connect.consultar_datos_del_producto,
     "total_pedidos_cliente": connect.consultar_pedidos_clientes,
+    
+    # Zona de todos
     "todos_clientes": connect.todos_clientes,
-    "todos_pedidos": connect.todos_pedidos
+    "todos_pedidos": connect.todos_pedidos,
+    "todos_productos": connect.todos_productos,
+
+    "cliente_borrar": connect.eliminar_cliente_por_id,
+    "producto_borrar": connect.eliminar_producto_por_id,
+    "pedido_borrar": connect.eliminar_pedido_por_id
+}
+
+modify_bdd = {
+    "agregar_cliente": connect.agregar_cliente,
+    "agregar_producto": connect.agregar_producto,
+    "agregar_pedido": "",
+    
+    "modificar_cliente": "",
+    "modificar_producto": "",
+    "modificar_pedido": ""
 }
 
 @app.route("/", methods=["GET"])
 def main_():
     return render_template("index.html")
+
+@app.route("/api/modify_bdd", methods=["POST"])
+def modify_():
+    parametros: dict = request.get_json()
+
+    if any(connect.es_peligroso(x) for x in parametros.keys()) or any(connect.es_peligroso(x) for x in parametros.values()):
+        print("es peligroso")
+
+        return {"status": 400, "data": "Estas utilizando caracteres no autorizados."}
+
+    if "consulta" not in parametros or "data" not in parametros:
+        return {"status": 400, "data": "No estas enviando uno de los dos parametros con su respectivo valor."}
+    
+    if parametros["consulta"] not in modify_bdd:
+        return {"status": 500, "data": "El valor del parametro 'consulta' no existe como consulta."}
+    
+    resp_ = modify_bdd[parametros["consulta"]](parametros["data"])
+    
+    return resp_
 
 @app.route("/api/dinamic_api", methods=["GET", "POST"])
 def dinamic_():
@@ -31,12 +67,15 @@ def dinamic_():
     
     if parametros["consulta"] not in diccionario_de_busquedas:
         return {"status": 500, "data": "El valor del parametro 'consulta' no existe como consulta."}
+    
+    if any(parametros["consulta"] == x for x in ["todos_clientes", "todos_pedidos", "todos_productos"]):
+        respuesta_de_la_base_de_datos = diccionario_de_busquedas[parametros["consulta"]]()
 
-    respuesta_de_la_base_de_datos = diccionario_de_busquedas[parametros["consulta"]](parametros["data"])
+    else:
+        respuesta_de_la_base_de_datos = diccionario_de_busquedas[parametros["consulta"]](parametros["data"])
     
     return respuesta_de_la_base_de_datos
 
 if __name__ == "__main__":
-
     app.run(debug=True, use_reloader=False)
     
--- connect.py
+++ connect.py
@@ -5,6 +5,7 @@ bdd = connect["empresa"]
 
 clientes = bdd["clientes"]
 pedidos = bdd["pedidos"]
+productos = bdd["productos"]
 
 no_permitido = r"'\"\\/<>(){}[];%*=$+|&`~#"
 
@@ -19,6 +20,8 @@ def es_peligroso(campo: str) -> bool:
             return True
     
     return False
+
+########################################### 
 
 ########################################### Requerimientos basicos
 
@@ -38,7 +41,7 @@ def busqueda_cliente_ciudad(ciudad: str) -> dict:
 def busqueda_por_fecha(fecha: str) -> dict:
 
     try:
-        resp_ = clientes.find({"fecha_registro": fecha}, {"_id": 0, "nombre": 1})
+        resp_ = list(clientes.find({"fecha_registro": {"$regex": fecha}}, {"_id": 0, "nombre": 1}))
     
     except:
         print("ERROR: en busqueda por fecha")
@@ -47,29 +50,30 @@ def busqueda_por_fecha(fecha: str) -> dict:
     if not resp_:
         return {"status": 000, "data": f"No se lograron encontrar resultados de busqueda por la fecha: {fecha}"}
     
-    return {"status": 200, "data": list(resp_)}
+    return {"status": 200, "data": resp_}
 
 def consultar_datos_del_producto(id_producto: str) -> dict:
 
     try:
-        resp_ = pedidos.find({"_id": id_producto}, {"_id": 0})
+        resp_ = list(productos.find_one({"_id": id_producto}, {"_id": 0}))
 
     except:
         print("ERROR: en busqueda por id de producto")
-        return {"status": 500, "data": "INTERNAL ERROR SERVER"}
+        return {"status": 500, "data": f"No se logro encontrar el producto con id: {id_producto}"}
 
     if not resp_:
         return {"status": 000, "data": f"No se logro encontrar ningun producto por le id: {id_producto}"}
     
-    return {"status": 200, "data": list(resp_)}
+    return {"status": 200, "data": resp_}
 
 def consultar_pedidos_clientes(id_cliente: str):
     
     try:
-        resp_ = clientes.aggregate([
-            {"$lookup": {"from": "pedidos", "localField": "_id", "foreignField": "cliente_id", "as": "pedidos_del_cliente"}},
+        resp_ = list(clientes.aggregate([
+            {"$match": {"_id": id_cliente}},
+            {"$lookup": {"from": "pedidos", "localField": "_id", "foreignField": "codigo_cliente", "as": "pedidos_del_cliente"}},
             {"$project": {"_id": 0, "pedidos_del_cliente.cliente_id": 0}}
-        ])
+        ]))
     except:
         print("ERROR: en busqueda por id cliente en consultar pedidos cliente")
         return {"status": 500, "data": "INTERNAL ERROR SERVER"}
@@ -77,9 +81,9 @@ def consultar_pedidos_clientes(id_cliente: str):
     if not resp_:
         return {"status": 000, "data": f"No se logro encontrar ningun pedido asociado al id: {id_cliente}"}
     
-    return {"status": 200, "data": list(resp_)}
+    return {"status": 200, "data": resp_}
 
-def todos_clientes(arg_: str) -> dict:
+def todos_clientes() -> dict:
 
     try:
         resp_ = list(clientes.find({}))
@@ -89,8 +93,7 @@ def todos_clientes(arg_: str) -> dict:
     
     return {"status": 200, "data": resp_}
 
-# Alta paja modificar y agregar except, mejor un parametro invisible que no se ocupa
-def todos_pedidos(arg_: str) -> dict:
+def todos_pedidos() -> dict:
 
     try:
         resp_ = list(pedidos.find({}))
@@ -99,3 +102,100 @@ def todos_pedidos(arg_: str) -> dict:
         return {"status": 500, "data": "INTERNAL ERROR SERVER"}
     
     return {"status": 200, "data": resp_ }
+
+def todos_productos() -> dict:
+
+    try:
+        resp_ = list(productos.find({}))
+    except:
+        print("ERROR: en busqueda por todos los pedidos")
+        return {"status": 500, "data": "INTERNAL ERROR SERVER"}
+    
+    return {"status": 200, "data": resp_ }
+
+########################################################
+
+def agregar_cliente(json_data: dict) -> dict:
+
+    telefono = json_data["telefono"]
+    direccion = json_data["direccion"]
+    email = json_data["email"]
+    fecha_registro = json_data["fecha"]
+    nombre = json_data["nombre"]
+
+    resp_ = todos_clientes()
+    id_cliente = "C00" + str(len(resp_["data"]) + 1)
+
+    try:
+        clientes.insert_one({
+            "_id": id_cliente, 
+            "Telefono": telefono, 
+            "direccion": direccion, 
+            "email": email, 
+            "fecha_registro": fecha_registro,
+            "nombre": nombre
+        })
+    
+    except:
+        print(f"ERROR: No se logro insertar al usuario: {nombre} correctamente")
+        return {"status": 500, "data": f"No se pudo agregar al nuevo usuario con nombre {nombre}."}
+
+    return {"status": 200, "data": f"Usuario {nombre} ingresado correctamente."}
+
+def agregar_producto(json_data: dict) -> dict:
+    
+    nombre = json_data["nombre"]
+    precio = json_data["precio"]
+    stock = json_data["stock"]
+    estado = json_data["estado"]
+
+    resp_ = todos_productos()
+    id_producto = "PD00" + str(len(resp_["data"]) + 1)
+
+    try: 
+        productos.insert_one({
+            "_id": id_producto,
+            "nombre": nombre,
+            "precio": int(precio),
+            "stock": int(stock),
+            "estado": estado
+        })
+
+    except:
+        print(f"ERROR: No se logro insertar al producto: {nombre} correctamente")
+        return {"status": 500, "data": f"No se pudo agregar al nuevo producto con nombre {nombre}."}
+
+    return {"status": 200, "data": f"Producto {nombre} ingresado correctamente."}
+
+def eliminar_cliente_por_id(id):
+    
+    try:
+        clientes.delete_one({"_id": id})
+    
+    except:
+        print(f"ERROR: No se logro eliminar al usuario: {id} correctamente")
+        return {"status": 500, "data": f"Usuario con id {id} no eliminado."}
+
+    return {"status": 200, "data": f"Usuario con id {id} eliminado correctamente."}
+
+def eliminar_pedido_por_id(id):
+
+    try:
+        pedidos.delete_one({"_id": id})
+    
+    except:
+        print(f"ERROR: No se logro eliminar el pedido: {id} correctamente")
+        return {"status": 500, "data": f"Pedido con id {id} no eliminado."}
+
+    return {"status": 200, "data": f"Pedido con id {id} eliminado correctamente."}
+
+def eliminar_producto_por_id(id):
+    
+    try:
+        productos.delete_one({"_id": id})
+    
+    except:
+        print(f"ERROR: No se logro eliminar al producto: {id} correctamente")
+        return {"status": 500, "data": f"Producto con id {id} no eliminado."}
+
+    return {"status": 200, "data": f"Producto con id {id} eliminado correctamente."}
